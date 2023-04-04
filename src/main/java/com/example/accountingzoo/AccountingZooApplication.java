a7eb3e1c2fa72711e4cd5fb808c9f67e5ec96df8
romanov <romanov@evolenta.ru>
2023-04-04 16:39:01 +0300

    init

---
+++ src/main/java/com/example/accountingzoo/AccountingZooApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountingzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingZooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingZooApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/accountingzoo/model/Animal.java
@@ -0,0 +1,14 @@
+package com.example.accountingzoo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+@Data
+@Entity
+public class Animal {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+}
